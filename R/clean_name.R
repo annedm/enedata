b89e90cbb4fbba7de1606e7ea30de4f67e293d7e
Scrubz00 <younes.chahma@gmail.com>
2020-11-03 14:41:41 +0100

    ecriture de la fonction clean_name

--- R/clean_name.R
+++ R/clean_name.R
@@ -4,15 +4,17 @@
 #'
 #' @return chaine de caractère avec une majuscule en premier, plus d espace superflu
 #' @export
+#' @import stringr
 #' @examples
 #' clean_name(' il y  a des espaces    en trop! ')
 clean_name <- function(name){
   
   ##supprimer les blancs en trop 
-  name <- name  
+  name <- str_trim() %>% 
+    str_replace_all(pattern = "\\s+", replacement = " ")
   
   ##mettre la premiere lettre seulement en majuscule
-  
+  substr(name, 1, 1) <- toupper(substr(name, 1, 1))
   
   name
 }
