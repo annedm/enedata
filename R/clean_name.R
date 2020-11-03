4d7f495f7a5fb30f7e55a263db1d716064110dca
anne de moliner <annedemoliner@gmail.com>
2020-11-03 14:40:46 +0100

    clean name et get_conso_an

--- R/clean_name.R
+++ R/clean_name.R
@@ -4,15 +4,18 @@
 #'
 #' @return chaine de caractère avec une majuscule en premier, plus d espace superflu
 #' @export
+#' @import magrittr
+#' @importFrom stringr str_to_title str_trim str_replace str_replace_all
 #' @examples
 #' clean_name(' il y  a des espaces    en trop! ')
 clean_name <- function(name){
-  
-  ##supprimer les blancs en trop 
-  name <- name  
+  name <- name %>%
+    str_replace_all(pattern ='\\s+', replacement = ' ') %>%
+    str_trim() 
   
   ##mettre la premiere lettre seulement en majuscule
-  
+  name <- paste0(toupper(substr(name,1, 1)), substr(name, 2, nchar(name)))
+         
   
   name
 }
--- R/get_conso_an_commune.R
+++ R/get_conso_an_commune.R
@@ -2,32 +2,48 @@
 #' d'une commune
 #'
 #' @param annee entier, annee a recuperer (si missing, toutes)
+#' @param commune character, commune a recuperer
 #' @param verbose booleen qui indique si on print l'url
 #' @param with_coord booleen, est ce qu'on doit récupérer les coordonnées
 #'
 #' @return data frame avec en ligne les données de consommation par commune x segment de clientèle
 #' @export
+#' @importFrom assertthat assert_that
+#' @importFrom httr GET http_error http_status
+#' @importFrom rjson fromJSON
+#' @import dplyr
+#' @import magrittr
 #' @examples
 #' get_conso_an_commune(annee = 2016, commune = 'Valenciennes')
 #' get_conso_an_commune( commune = 'paris')
 
 get_conso_an_commune <- function(annee,
-                                 commune ,
-                                 with_coord = FALSE,
-                                 verbose = FALSE){
+                     commune ,
+                     with_coord = FALSE,
+                     verbose = FALSE){
   
+
+  ##check des arguments
+  if (!missing(annee)){
+    assert_that(as.integer(annee) == annee)
+    
+  }
+   assert_that(is.character(commune))
+  assert_that(length(commune) == 1)
   
-  ##check des arguments : TODO
-  
-  
-  ## TODO mise en forme de la commune ('paris' doit passer) 
+  ## mise en forme de la commune
   commune <- clean_name(commune)
   
-  ##TODO : changer l url pour recuperer la bonne ville  et la bonne annee (ici c'est lille)
-  ##l url initiale 
-  adr <- 'https://data.enedis.fr/api/records/1.0/search/?dataset=consommation-electrique-par-secteur-dactivite-commune&q=&rows=-1&refine.nom_commune=Valenciennes'
-   
-
+  ##l url initiale
+  adr <- 'https://data.enedis.fr/api/records/1.0/search/?dataset=consommation-electrique-par-secteur-dactivite-commune&q=&rows=-1'
+  
+  
+  adr <- paste0(adr, '&refine.nom_commune=', commune)
+  
+  if (!missing(annee)){
+    adr <- paste0(adr, '&refine.annee=', as.character(annee))
+  }
+  
   if(verbose){
     print(paste0('Recuperation de l url : ', adr))
   }
@@ -36,28 +52,33 @@ get_conso_an_commune <- function(annee,
   
   brut <- GET(adr)
   
-  ##TODO: arret si il y a une erreur
-  
+  ##arret si il y a une erreur
+  if(http_error(brut)){
+    stop(http_status(brut))
+  }
   
   ##extraction du contenu et mise sous forme de dataframe
   contenu <- brut$content
-  
+ 
   liste <- fromJSON(rawToChar(contenu))
-  
-  ##TODO: supprimer les variables qui contiennent les geom.coordinates 
-  ##si pas demande 
+
   df_conso <- lapply(liste$records
                      ,FUN = function(rec){
                        
                        out <- as.data.frame(rec$fields) %>%
                          mutate_if(is.factor, as.character)
-                      
+                       
+                       if (!with_coord){
+                         out <- out %>% select(-contains('geom.coord'))
+                       }
                        
                        out
                      }) %>% bind_rows()
   
-  ##TODO warning si aucune donnees recuperee
- 
+  ##warning si aucune donnees recuperee
+  if(nrow(df_conso) == 0){
+    warning('Pas de donnees recuperees')
+  }
   
   df_conso
   
